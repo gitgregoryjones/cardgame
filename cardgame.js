6aed2458a72f6d8e3ef4d8c4ecbc001f7e61cc96
Gregory Jones <gregory.jones@turner.com>
2015-10-10 16:22:50 -0400

    refactor

--- cardgame.js
+++ cardgame.js
@@ -16,35 +16,31 @@ var DECK = {}
 
 
 function Deck(suites, cards){
-	rdeck = [];
+	this.rdeck = [];
 	suites.forEach(function(suite){
 		cards.forEach(function(card){
 			console.log("Building "+suite + " : " + card );
-			rdeck.push(new Card(suite,card))
+			this.rdeck.push(new Card(suite,card))
 		})
 	})
-	return rdeck;
+
+	this.shuffle = function(){
+		var i = 0, j = 0, temp = null;
+		for (i = array.length - 1; i > 0; i -= 1) {
+		    j = Math.floor(Math.random() * (i + 1))
+		    temp = array[i]
+		    array[i] = array[j]
+		    array[j] = temp
+		}
+	}
+
+	this.nextCard = function(){
+		return this.rdeck.pop();
+	}
+
+	return this;
 }
 
-
-
-function shuffle (array) {
-  var i = 0
-    , j = 0
-    , temp = null
-
-  for (i = array.length - 1; i > 0; i -= 1) {
-    j = Math.floor(Math.random() * (i + 1))
-    temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
-  }
-}
-
-function dealCard(gameName){
-	
-	return gameName.pop();
-}
 
 function Card(suite,number){
 	this.suite = suite;
