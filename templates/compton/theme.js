a745813fe34a7ea3bbe62268f632e8ba26e942c6
Gregory Jones <gregory.jones@turner.com>
2015-10-11 13:32:04 -0400

    update theme

--- templates/compton/theme.js
+++ templates/compton/theme.js
@@ -3,7 +3,7 @@ themes["compton"] = new Theme(
 		[
 			"https://rawgit.com/geegit/cardgame/master/templates/compton/sound/cockgun-327068561.mp3",
 			"https://rawgit.com/geegit/cardgame/master/templates/compton/sound/Gun-554065581.mp3",
-			"https://rawgit.com/geegit/cardgame/master/templates/compton/sound/yell.mp3"
+			"https://dl-web.dropbox.com/get/Public/yell.mp3?_subject_uid=68730304&w=AAC4o3Obt5_8JiE32Y9VYoVOEjK0EY9LR_1H6NPDc-0UDg"
 		],
 
 	"https://rawgit.com/geegit/cardgame/master/templates/compton/compton_cover.png"
