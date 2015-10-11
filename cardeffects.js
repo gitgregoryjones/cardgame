e2931344def1c48e1ac6feaebb3938b80c5ffb93
Gregory Jones <gregory.jones@turner.com>
2015-10-11 11:22:26 -0400

    Add cardeffects

--- cardeffects.js
+++ cardeffects.js
@@ -4,11 +4,11 @@ function colorCardText(cardSuite)
 
 	if(cardSuite.indexOf("hearts") > -1 || cardSuite.indexOf("diamonds") > -1 ){
 
-		$('#cache1').css('color','red');
 		$('[data-label="PlayerCard"] span').css('color','red');
+		$('[data-label="RenderCard"] span').css('color','red');
 	} else {
-		$('#cache1').css('color','black')
-		$('[data-label="PlayerCard"] span').css('color','black')
+		$('[data-label="PlayerCard"] span').css('color','black');
+		$('[data-label="RenderCard"] span').css('color','black')
 	}
 
 }
