208040a9653b4b2414ed4e1e3ccf0cd891faa4de
gitgregoryjones <gregory.jones@synovas.com>
2025-08-07 23:25:09 -0400

    Update cardgame.js

--- cardgame.js
+++ cardgame.js
@@ -13,7 +13,11 @@ var cards = [2,3,4,5,6,7,8,9,10,11,12,13,14];
 
 function Deck(){
 	var rdeck = [];
+   if(isActive("JIRA-93030")){
 	console.log(this.rdeck)
+ 	} else {
+	console.log("negative condition");
+  }
 
 	suites.forEach(function(suite){
 		cards.forEach(function(card){
